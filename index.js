cb9de0bf88dff03ba7486186ccb1ecbc9adb504c
Sayanee <sayanee@gmail.com>
2014-03-25 15:02:47 +0800

    added socket.io in both server and client

--- index.js
+++ index.js
@@ -27,10 +27,11 @@ app.requestAfterRoute = function requestAfterRoute(server) {
 
 
 if (require.main === module) {
-    kraken.create(app).listen(function (err) {
+    kraken.create(app).listen(function (err, server) {
         if (err) {
             console.error(err.stack);
         }
+        var io = require('./lib/socket').listen(server);
     });
 }
 
